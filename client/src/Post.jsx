f999d2f16e59a916c6398574a02f2c52eeeb42c6
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-11 16:13:47 +0530

    firstcommit

---
+++ client/src/Post.jsx
@@ -0,0 +1,16 @@
+export default function Post(){
+    return(
+  <div className="post">
+    <div className="image">
+    </div>
+    <div className="texts">
+      <h2>Full-house battery backup coming</h2>
+      <p className="info">
+        <a className="author">Isha Patel</a>
+        <time>11-06-2024 13:32pm</time>
+      </p>
+      <p className="summary">XYXZFNJEEINSINVJNJGNJNG V  VNNJ</p>
+    </div>
+  </div>
+    );
+  }
