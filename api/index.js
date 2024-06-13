538c26829fec1948adfed800f22df358c6ffbcfe
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-14 01:54:22 +0530

    error 2

--- api/index.js
+++ api/index.js
@@ -4,11 +4,16 @@ const mongoose=require("mongoose");
 const User=require('./models/User');
 const bcrypt=require('bcryptjs');
 const app=express();
+const  jwt=require('jsonwebtoken');
+const cookieParser=require('cookie-parser');
 
 const salt=bcrypt.genSaltSync(10);
+const secret='adfbijcnidjnvsjfnfonsdfmsivsfvn';
 
-//app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:5173/'}));
 app.use(express.json());
+app.use(cookieParser());
+
 
 mongoose.connect('mongodb+srv://blog:blogapp7@cluster7.9isolbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster7');
 
@@ -31,7 +36,30 @@ app.post('/api/login',async (req,res)=>{
 const {username,password}=req.body;
 const userDoc= await User.findOne({username});
 const passOk = bcrypt.compareSync(password,userDoc.password);
-res.json(userDoc);
+if(passOk){
+    //logged in
+jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+if(err) throw err;
+res.cookie('token',token).json('ok');
+});
+
+//res.json()
+}
+else{
+    res.status(400).json('wrong credentials');
+}
+})
+
+app.get('/api/profile',(req,res)=>{
+    const {token}=req.cookies;
+jwt.verify(token,secret,{},(err,info)=>{
+    if(err) throw err;
+    res.json(info);
+});
+});
+
+app.post('/api/logout',(req,res)=>{
+    res.cookie('token','').json('ok');
 })
 
 app.listen(4000);
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,14 +1,44 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Header() {
+  const[username,setUsername]=useState(null);
+  useEffect(()=>{
+fetch('/api/profile',{
+  credentials:'include',
+}).then(response=>{
+response.json().then(userInfo=>{
+setUsername(userInfo.username);
+})
+});
+  },[]);
+
+function logout(){
+  fetch('/api/logout',{
+    credentials:'include',
+    method:'POST',
+  })
+}
+
   return (
     <header>
       <Link to="/" className="logo">
         MyBlog
       </Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+          <Link to="/api/create">Create new post</Link>
+          <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+<Link to="/login">Login</Link>
+<Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
